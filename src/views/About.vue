d9e7a7440d48a3587467bb41c53ebe9f276527ab
Sadaffi <bartek_sadowski@o2.pl>
2021-10-23 14:51:50 +0200

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Hello from "@/components/Hello.vue";
+
+const name = "About";
+</script>
+
+<template>
+  <div class="about">
+    <Hello :name="name" />
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Hello from "@/components/Hello.vue";
+
+const name = "Home";
+</script>
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Hello :name="name" />
+  </div>
+</template>
