4e5aaaaf469fc7e9315fb7cc2013ab12b3a5e199
Sadaffi <bartek_sadowski@o2.pl>
2021-11-03 22:16:46 +0100

    [Login Page] Added basic login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,120 @@
 <script setup lang="ts">
-import Hello from "@/components/Hello.vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { authenticationStore } from "@/store/authentication";
+import { MAIN } from "@/dicts/routes";
+import * as rules from "@/utils/rules";
 
-const name = "Login";
+const credentialsBoilerplate = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const router = useRouter();
+
+(function onBeforeRouteEnter() {
+  const { isLoggedIn } = authenticationStore.getState();
+
+  if (isLoggedIn) {
+    router.push({ name: MAIN });
+  }
+})();
+
+const formRef = ref(null);
+const isRegister = ref(false);
+const credentials = ref({ ...credentialsBoilerplate });
+
+function changeIsRegister() {
+  isRegister.value = !isRegister.value;
+
+  credentials.value = { ...credentialsBoilerplate };
+}
+
+async function onSubmit() {
+  if (!(await formRef.value.validate())) {
+    return;
+  }
+
+  const { email, password } = credentials.value;
+
+  authenticationStore.authenticate(email, password).then((isAuthenticated) => {
+    if (isAuthenticated) {
+      router.push({ name: MAIN });
+    }
+  });
+}
 </script>
 
 <template>
-  <div class="home">
-    <Hello :name="name" />
-  </div>
+  <q-page class="row justify-center items-center">
+    <div class="row">
+      <q-card square bordered class="q-pa-lg bg-blue-1 shadow-1">
+        <q-card-section>
+          <q-form ref="formRef" class="q-gutter-md">
+            <q-input
+              v-model="credentials.email"
+              type="email"
+              label="email"
+              :rules="[rules.isEmpty(), rules.isEmail()]"
+              square
+              filled
+              clearable
+            />
+            <q-input
+              v-model="credentials.password"
+              type="password"
+              label="hasło"
+              :rules="[rules.isEmpty(), rules.isValidPassword()]"
+              square
+              filled
+              clearable
+            />
+            <q-input
+              v-if="isRegister"
+              v-model="credentials.confirmPassword"
+              type="password"
+              label="potwierdź hasło"
+              :rules="[rules.isEmpty(), rules.isSame(credentials.password)]"
+              square
+              filled
+              clearable
+            />
+          </q-form>
+        </q-card-section>
+        <q-card-actions class="q-px-md">
+          <q-btn
+            class="full-width"
+            label="Login"
+            unelevated
+            color="primary"
+            size="lg"
+            @click="onSubmit"
+          />
+        </q-card-actions>
+        <q-card-section class="text-center q-pa-none">
+          <p
+            v-if="isRegister"
+            class="text-grey-6 cursor-pointer"
+            @click="changeIsRegister"
+          >
+            Posiadasz już konto? Zaloguj się
+          </p>
+          <p
+            v-else
+            class="text-grey-6 cursor-pointer"
+            @click="changeIsRegister"
+          >
+            Nie posiadasz konta? Zarejestruj się
+          </p>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
 </template>
+
+<style>
+.q-card {
+  width: 360px;
+}
+</style>
